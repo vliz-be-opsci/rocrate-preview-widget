3680ef08c5bf65b49c9a0187834d44ef44f2665f
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2022-05-24 18:56:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,15 @@ time=$(date)
 echo "::set-output name=time::$time"
 pwd
 ls -a
+echo "making tocopy dir in src"
+mkdir ./src/topcopy
+echo "copying everything in current dir into tocopy"
+rsync --recursive --progress --exclude=".* .src" ./ ./src/tocopy
 echo "installing dependencies for building react app"
 npm install
 echo "npm run build"
 npm run build
 echo "copying over scr files to build folder"
-cp ./scr/tocopy/* ./build/ -r
+cp ./src/tocopy/* ./build/ -r
 echo "renaming index.html in the build folder to ro-crate-preview.html"
 mv ./build/index.html ./build/ro-crate-preview.html
