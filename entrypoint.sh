7be7bf6008003e742af445f9cd420fd1437254fd
Rory <rory.meyer@vliz.be>
2022-05-24 15:20:36 +0200

    made entrypoint +X

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-#STEPS:
-# Copy the rocrate files to .../src/tocopy
-# run "npm run predeploy" or "npm run build"
-# move ../build* to gh-pages branch
-echo "hello world"
+#!/bin/sh -l
+
+echo "Hello $1"
+time=$(date)
+echo "::set-output name=time::$time"
