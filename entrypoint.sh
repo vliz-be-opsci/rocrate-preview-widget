652cf03608cf195fa6c1e43b7903dc6b73f1c6fa
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2022-05-24 15:47:25 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,9 @@
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
+echo "npm run build"
+npm run build
+echo "copying over scr files to build folder"
+cp ./scr/tocopy/* ./build/ -r
+echo "renaming index.html in the build folder to ro-crate-preview.html"
+mv ./build/index.html ./build/ro-crate-preview.html
