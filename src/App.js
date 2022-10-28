3613abecac8fb016641b9588352b2841179ee923
cedricdecruw <30471340+cedricdcc@users.noreply.github.com>
2022-10-28 11:41:43 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,162 @@
 import './App.css';
-import HomePage from './pages/homepage/new_homepage';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-
+import React from 'react';
+import Layout from './Layouts/normal_layout';
+import { getRocrateMetadata, parseJsonld, getDatasets } from './Services/Constants/constants';
+import SideBar from './Components/Sidebar/Sidebar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { push as Menu } from 'react-burger-menu';
+import {BiGitBranch} from 'react-icons/bi';
+import {useState, useEffect} from 'react';
 function App() {
-  //return statement
+  console.log(getRocrateMetadata);
+  const [currentobjectselected, setCurrentObjectSelected] = useState("");
+  //call the parseJsonld function and pass the rocrate-metadata.json file as a parameter
+  //the parseJsonld function will return the rdf triples
+  
+  
+  parseJsonld(getRocrateMetadata).
+  then((rdf_version_jsonld) => {
+    console.log(rdf_version_jsonld);
+  }).catch((err) => {
+    console.log(err);
+  });
+  
+  
+  let all_datasets = getDatasets(getRocrateMetadata);
+
+  const getTreeData = (props) => {
+		//get the @graph property from the props
+		const graph = props["@graph"];
+		//console.log(graph);
+		//create an empty array to store the datasets
+		let treedata = [];
+		//loop through all the items in the @graph property and check if the item is a File and also not an url
+		let intermediate_data = [];
+		graph.forEach((item) => {
+			if (item["@type"] === "File" && item["@id"].indexOf("http") === -1) {
+				//if the item is a File and not an url, then push the item to the treedata array
+				//split the item by the / and get the last item in the array
+				//this will be the name of the file
+				const filename = item["@id"].split("/").pop();
+				//split the item by the / and loop over the array 
+				//and push the items to the treedata array
+				const file_path = item["@id"].split("/");
+				//loop over file_path 
+				for(let i = 0; i < file_path.length; i++) {
+					//check if the item is the last item in the array
+					const full_path_name = file_path.slice(0, i+1).join("/");
+					if (i === file_path.length - 1) {intermediate_data.push({"name":filename,"type":"file","level":i,"parent":file_path[i-1],"full_name":full_path_name});}
+					else {
+						try {
+
+							intermediate_data.push({"name":file_path[i],"type":"folder","level":i,"parent":file_path[i-1],"full_name":full_path_name});
+						} catch (error) {
+							console.log(error);
+						}
+					}
+				}
+			}
+		})
+		console.log(intermediate_data);
+		//make an array where all dict are sorted by level from high to low
+		let sorted_data = intermediate_data.sort((a,b) => (a.level > b.level) ? -1 : ((b.level > a.level) ? 1 : 0));
+		
+		//delete all the items that are level 0
+		sorted_data = sorted_data.filter((item) => item.level !== 0);
+		// filter out all the duplicate items
+		sorted_data = sorted_data.filter((item, index, self) => index === self.findIndex((t) => (t.name === item.name && t.level === item.level && t.parent === item.parent)));
+		console.log(sorted_data);
+		let reverse_construct = [];
+		//loop over the sorted_data array and construct the tree
+		sorted_data.forEach((item) => {
+			console.log("file");
+			//check if the parent of the file is in the construct array
+			if (reverse_construct.filter((construct) => construct.name === item.parent).length > 0) {
+				//if the parent is in the construct array, then add the file to the parent
+				reverse_construct.forEach((construct) => {
+					if (construct.name === item.parent) {
+						construct.content.push(item.name);
+					}
+				})
+			}else{
+				//if the parent is not in the construct array, then add the parent and the file to the construct array
+				reverse_construct.push({"name":item.parent,"content":[item.name]});
+			}
+		})
+		console.log(reverse_construct);
+		//loop over the reverse_construct array, get the item.name and check if this item.name is present in any of the other reverse_construct items.content
+		//if the item.name is present in any of the other reverse_construct items.content, then add the item to the content of the other item
+		reverse_construct.forEach((item) => {
+			var found = false;
+			reverse_construct.forEach((construct) => {
+				if (construct.content.includes(item.name)) {
+					found = true;
+					construct.content.push(item);
+					construct.content = construct.content.filter((content) => content !== item.name);
+				}
+			})
+			if (found === true) {
+				reverse_construct = reverse_construct.filter((construct) => construct.name !== item.name);
+			}
+		});
+		console.log(reverse_construct);
+		treedata.push({"name":".","content":reverse_construct[0].content});
+
+		//now we get all the items from the graph that are urls
+		//loop through all the items in the @graph property and check if the item is a File and also not an url
+		let resource_data = [];
+		graph.forEach((item) => {
+			if (item["@id"].indexOf("http") !== -1) {
+				resource_data.push(item["@id"]);
+			}
+		})
+		treedata.push({"name":"Resources","content":resource_data});
+		setTreeInfo(treedata);
+		setOriginalTree(treedata);
+		setFullSortedData(sorted_data);
+	}
+
+	useEffect(() => {
+		getTreeData(getRocrateMetadata);
+	}, [getRocrateMetadata])
+
+  const [treeinfo, setTreeInfo] = useState([]);
+	const [searchterm, setSearchTerm] = useState("");
+	const [full_sorted_data, setFullSortedData] = useState([]);
+	const [originaltree, setOriginalTree] = useState([]);
+
+
   return (
-    <>
-      <div className="App">
-        <div>
-            <HomePage/>
+    <div id="App">
+      <Layout>
+        <div className="App" id="outer-container">
+          <SideBar 
+            setCurrentObjectSelected={setCurrentObjectSelected} 
+			      currentobjectselected={currentobjectselected}
+            rocrateinfo={getRocrateMetadata} 
+            treeinfo={treeinfo} 
+            setTreeInfo={setTreeInfo} 
+            searchterm={searchterm} 
+            setSearchTerm={setSearchTerm} 
+            full_sorted_data = {full_sorted_data}
+			      originaltree = {originaltree}
+          />
+          <div id="page-wrap" className='notSideBar'>
+            <div className='main_window_component'>
+              <div>
+                {currentobjectselected}
+                <br></br>
+                <a href={currentobjectselected} target="_blank" download>{currentobjectselected.split("/").pop()}</a>
+              </div>
+            </div>
+          </div>
+          <div className='mini_dashboard'>
+            <div className='gitbranchicon'><BiGitBranch/></div>
+          </div>
         </div>
-      </div>
-    </>
+      </Layout>
+    </div>
   );
 }
-
 
 export default App;
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,165 +1,36 @@
-//3th party imports here
-import React, {useEffect,useState} from 'react';
-import {AiFillFolderOpen, AiOutlineLink} from 'react-icons/ai';
-import {BsFillFileEarmarkBreakFill} from 'react-icons/bs';
-import {VscRootFolder} from 'react-icons/vsc';
-import {IoMdArrowDropleft} from 'react-icons/io';
-import {RiFolderReceivedFill} from 'react-icons/ri';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
-//component imports here
-import FileIconElement from '../file_icon_element/file_icon_element';
+import React from 'react';
+import { push as Menu } from 'react-burger-menu';
+import Tree from '../Tree/Tree';
+import { useState, useEffect } from 'react';
 
-//import util functions here
-import {check_name_file_display } from '../../utils/rocrate_metadata_functions';
+const SideBar = (props) => {
+	console.log(props);
 
-//css import here
-import './sidebar.css';
+	//function that will be called when the user types in the search bar
+	const handleSearch = (event) => {
+		//get the value of the search bar
+		const search_term = event.target.value;
+		//set the search term to the value of the search bar
+		props.setSearchTerm(search_term);
+		//if the search term is empty, then set the tree info to the folder info
+		if (search_term === "") {
+			props.setTreeInfo(props.treeinfo);
+		}
+	}
 
+  return (
+    <>
+    <Menu pageWrapId={'page-wrap'} outerContainerId={ "outer-container" } width={ '22%' }>
+		<h1>Rocrate Contents</h1>
+		<div className='flex'>
+			<input type="text" placeholder="Search" onChange={(e) => handleSearch(e)} />
+			<button onClick={() => props.setTreeInfo(props.originaltree)}>Reset</button>
+		</div>
+      {Tree(props.treeinfo,props.currentobjectselected,props.setCurrentObjectSelected,props.setTreeInfo,props.originaltree)}
+    </Menu>
+    </> 
+    
+  );
+};
 
-function Sidebar(props) {
-    //constants
-    const metadata = props.metadata;
-    const dataFiles = props.dataFiles;
-    const currentdirectory = props.currentdirectory;
-    const lastdirectory = props.lastdirectory;
-    const setLastDirectory = props.setLastDirectory;
-    const setCurrentDirectory = props.setCurrentDirectory;
-    const dataFilePaths = props.dataFilePaths;
-    const setSelectedFile = props.setSelectedFile;
-    const setCurrentBreadcrumb = props.setCurrentBreadcrumb;
-    const currentbreadcrumb = props.currentbreadcrumb;
-    const lastbreadcrumb = props.lastbreadcrumb;
-    const setLastBreadcrumb = props.setLastBreadcrumb;
-    const setHash = props.setHash;
-
-    //perform functions here
-    //child function that will determine if there is a go back button and a ./ button
-    function GoBack(props) {
-        var unique = props.dir_paths;
-        console.log(unique);
-        //if lastdirectory == "" then there is no go back button
-        if(currentbreadcrumb == "./") {
-            return (<></>);
-        }
-        // split the current breadcrumb by / and keep everything but the second and last and then join them with /
-        var split_current_breadcrumb = currentbreadcrumb.split('/');
-        var joined_current_breadcrumb = split_current_breadcrumb.slice(0, split_current_breadcrumb.length - 2).join('/')+'/';
-        if(joined_current_breadcrumb == "./") {
-            return (<></>);
-        }
-
-        return(
-            <button className="folderbutton navbarbutton" onClick={() => {
-                //set the current directory to the root directory
-                setCurrentDirectory(joined_current_breadcrumb);
-                //set the last directory to the current directory
-                setLastDirectory(currentdirectory);
-                setSelectedFile("");
-                setLastBreadcrumb(currentbreadcrumb);
-                setCurrentBreadcrumb(joined_current_breadcrumb);
-                setHash(joined_current_breadcrumb);
-            }}><RiFolderReceivedFill></RiFolderReceivedFill>: /(parent)</button>
-        );
-    }
-
-    function Currentnavigation() {
-        var unique_paths = [];
-        //loop over the metadata grapths and get all the dirs of the currentdirectry
-        console.log(String(currentdirectory));
-        let new_currentdirectory = String(currentdirectory);
-        //check if currentdirectory starts with "./" => if true then remove the "./"
-        if(currentdirectory.startsWith("./")) {
-            let new_currentdirectory = currentdirectory.substring(2);
-            console.log(new_currentdirectory);
-            // if length new_currentdirectory is 0 then set new_currentdirectory to currentdirectory
-            if(new_currentdirectory.length == 0) {
-                new_currentdirectory = currentdirectory;
-            }
-        }
-
-        console.log(metadata)
-        for (let i = 0; i < metadata["@graph"].length; i++) {
-            //if the current directory is the same as the current directory then add it to the unique_paths array
-            if (metadata["@graph"][i]["@id"] == new_currentdirectory) {
-                //look into the hasparts and get all the dirs
-                for (let j = 0; j < metadata["@graph"][i]["hasPart"].length; j++) {
-                    console.log(metadata["@graph"][i]["hasPart"][j]);
-                    //if last part is / then add to the unique_paths array
-                    if (metadata["@graph"][i]["hasPart"][j]["@id"].split("/")[metadata["@graph"][i]["hasPart"][j]["@id"].split("/").length - 1] === "") {
-                        unique_paths.push(metadata["@graph"][i]["hasPart"][j]["@id"]);
-                    }
-                }
-            }
-        }
-
-        //return a list of all the unique paths
-        console.log(currentbreadcrumb);
-        return (
-            <div className="currentnavigation">
-                <button className="folderbutton navbarbutton" onClick={() => {
-                    //set the current directory to the root directory
-                    setCurrentDirectory("./");
-                    //set the last directory to the current directory
-                    setLastDirectory(currentdirectory);
-                    setSelectedFile("");
-                    setLastBreadcrumb(currentbreadcrumb);
-                    setCurrentBreadcrumb('./');
-                    setHash('./');
-                }}><VscRootFolder></VscRootFolder>: /(root)</button>
-                <GoBack dir_paths = {unique_paths} currentbreadcrumb={currentbreadcrumb} setCurrentBreadcrumb={setCurrentBreadcrumb}/>
-                {unique_paths.map((unique_path) => (
-                    <OverlayTrigger
-                    placement="right"
-                    delay={{ show: 250, hide: 400 }}
-                    overlay={
-                        <Tooltip id="button-tooltip" {...props}>
-                        {unique_path}
-                        </Tooltip>
-                    }
-                    >
-                        <button className="folderbutton navbarbutton" onClick={() => {setLastBreadcrumb(currentbreadcrumb);setCurrentBreadcrumb(unique_path);setHash(unique_path); setCurrentDirectory(unique_path);setSelectedFile("");}}><AiFillFolderOpen></AiFillFolderOpen>: {unique_path}</button>
-                    </OverlayTrigger>
-                ))}
-                
-            </div>
-        );
-    }
-    //if loading is true return loading
-    return(
-        <>
-            <div class="containersidebar blue">
-                <div class="navbar_space">
-                    <div className="sidebar">
-                            <Currentnavigation/>
-                            {dataFilePaths.map((item, index) => {
-                                var folder_file = item["path"].split("/")[item["path"].split("/").length - 2];
-                                var file_id = item["path"].split("/")[item["path"].split("/").length - 1];
-                                console.log(folder_file);
-                                let new_currentdirectory = String(currentdirectory);
-                                //if currentdirecttory starts with "./" then remove the "./"
-                                if(currentdirectory.startsWith("./") && currentdirectory.length > 2) {
-                                    new_currentdirectory = currentdirectory.substring(2);
-                                }else{
-                                    new_currentdirectory = currentdirectory;
-                                }
-                                console.log(new_currentdirectory.split("/")[0]);
-                                if (folder_file == new_currentdirectory.split("/")[0]){
-                                    var toreturnfile_id = check_name_file_display(file_id);
-                                    return(
-                                        <>
-                                        <FileIconElement className="filebutton navbarbutton" setHash={setHash} currentbreadcrumb={currentbreadcrumb} setSelectedFile={setSelectedFile} file_id={file_id} toreturnfile_id={toreturnfile_id}></FileIconElement>
-                                        </>
-                                        
-                                    )
-                                }
-                            })}
-                            <button className='resourcebutton navbarbutton'><AiOutlineLink></AiOutlineLink>: resources here</button>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default Sidebar;
+export default SideBar;
