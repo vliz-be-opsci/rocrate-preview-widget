6a4b283eba0dc19be5ded32439dd27b42a7e1929
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2023-07-05 11:21:06 +0200

    added gh-repo to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,6 +2,7 @@
 
 export default function Footer(props: any) {
     //the empty p tag is to make sure the footer is at the bottom of the page
+    const reponame = props.reponame;
     return (
         <div className="footer">
             <p></p>
@@ -9,12 +10,16 @@ export default function Footer(props: any) {
                 <a href="https://github.com/vliz-be-opsci/rocrate-to-pages"> rocrate-to-pages </a> 
                 by 
                 <a href="https://open-science.vliz.be/" target="_blank">
-                    <img src="https://open-science.vliz.be/img/vliz-opsci-logo.png" alt="@vliz-be-opsci" className="footer_logo"/>
+                    <img src="https://open-science.vliz.be/img/VLIZ_LOGO.svg" alt="@vliz-be-opsci" className="footer_logo"/>
+                </a>
+                with data 
+                <a href={"https://github.com/"+reponame} target="_blank">
+                    <img src="https://open-science.vliz.be/img/github-logo.svg" alt="data-location" className="footer_logo"/>
                 </a>
             </p>
             <p>
                 <a href="./ro-crate-metadata.json">
-                    <img src="https://open-science.vliz.be/img/rdf.png" alt="ro-crate-metadata.json" className="footer_logo"/>
+                    <img src="https://open-science.vliz.be/img/rdf-logo.svg" alt="ro-crate-metadata.json" className="footer_logo"/>
                 </a>
             </p>
         </div>
